ab13891ffa3f7e263d9a554862ac5749d326941a
Kuzatuvchi <kuzatuvchi.my@gmail.com>
2024-12-19 16:24:41 +0500

    fix: installing packages

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import {App} from './App.tsx'
+import './index.css'
 import {DndProvider} from "react-dnd";
 import {HTML5Backend} from "react-dnd-html5-backend";
 
